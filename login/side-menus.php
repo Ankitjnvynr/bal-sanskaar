8e7c51c06f17274bc9230981ae2a6fad7817b82d
unknown <ankitbkana@outlook.com>
2024-06-28 22:01:34 +0530

    side bar tried of responsivenes

---
+++ login/side-menus.php
@@ -0,0 +1,21 @@
+<nav class="col-md-3 col-lg-2 d-none d-md-block   bg-danger sidebar">
+    <div class="position-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="#dashboard">Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#reports">Reports</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#settings">Settings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#profile">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#logout">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- login/studentdata.php
+++ login/studentdata.php
@@ -11,6 +11,7 @@
       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
       crossorigin="anonymous"
     />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .form-item{
             flex:1 0 230px;
@@ -19,9 +20,44 @@
         font-size: 0.9rem;
         }
     </style>
+    <style>
+        .sidebar {
+            min-height: 100vh;
+            padding-top: 20px;
+        }
+        .sidebar a {
+            padding: 10px 15px;
+            text-decoration: none;
+            font-size: 18px;
+            color: #ddd;
+            display: block;
+        }
+        .sidebar a:hover {
+            color: #fff;
+            background-color: #575d63;
+        }
+        
+    </style>
   </head>
 
   <body class="bg-warning-subtle">
+
+<div class="container-fluid">
+        <div class="row">
+            <?php
+              include 'side-menus.php'
+            ?>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="h2 text-center shadow-sm my-1 p-1 px-3 align-items-center  rounded-2 text-danger d-flex justify-content-between">
+                  <i class="fa-solid fa-bars d-md-none "></i>
+                
+                GIEO Gita-Bal Sanskaar</div>
+                <p>This is the main content area.</p>
+            </main>
+        </div>
+    </div>
+
+
 
 
     <script
