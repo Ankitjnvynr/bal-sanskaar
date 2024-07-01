984d5976e409f7dc3d9cf2842b996948d0735c3f
unknown <ankitbkana@outlook.com>
2024-07-01 22:13:11 +0530

    login page animatin with morpunkh

--- login/index.php
+++ login/index.php
@@ -62,7 +62,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="../css/login.css">
     <style>
         .form-item {
             flex: 1 0 230px;
@@ -80,10 +84,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 </head>
 
 <body class="bg-warning-subtle">
-
-    <div class="container h-full d-flex align-items-center justify-content-center">
+    <div class="leaf"></div>
+    <div style="--delay:1s; --x-pos:10%;" class="leaf"></div>
+    <div style="--delay:2s; --x-pos:70%;" class="leaf"></div>
+    <div style="--delay:3s; --x-pos:80%;" class="leaf"></div>
+    <div style="--delay:4s; --x-pos:30%;" class="leaf"></div>
+    <div style="--delay:5s; --x-pos:90%;" class="leaf"></div>
+    <div 
+        class="container h-full d-flex align-items-center justify-content-center">
         <!-- Login Form -->
-        <div id="loginForm" class="card p-4 shadow-sm col-md-4">
+        <div id="loginForm" style="background:transparent; backdrop-filter:blur(5px);"
+            class="card p-4 shadow-sm col-md-4">
             <div class="text-center fw-bold my-2 text-danger fs-3">Login</div>
             <form action="" method="post">
                 <div class="mb-3">
@@ -109,7 +120,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
                 echo $msg;
                 ?></div>
             </form>
-            <div class="text-center text-danger mt-3"><a class="btn" href="../"><i class="fa-solid fa-house"></i><span class="mx-2 fw-bold">Homepage-></span></a></div>
+            <div class="text-center text-danger mt-3"><a class="btn" href="../"><i class="fa-solid fa-house"></i><span
+                        class="mx-2 fw-bold">Homepage-></span></a></div>
         </div>
     </div>
 
