e9aa22e6281392b5aaf1cceadac4cb7b3e7239fb
unknown <ankitbkana@outlook.com>
2024-07-11 08:18:23 +0530

    center adding and auto increment added

--- login/centers.php
+++ login/centers.php
@@ -8,10 +8,9 @@
 
     if (isset($_POST['centerAdd']))
     {
-
         $newCenter = $_POST['newCenter'];
         $centerSql = "INSERT INTO centers (country, state, district, tehsil, center)
-    VALUES ('$centerCountry', '$centerState', '$centerDist', '$centerTeh', '$newCenter')";
+                      VALUES ('$centerCountry', '$centerState', '$centerDist', '$centerTeh', '$newCenter')";
         try
         {
             if ($result = $conn->query($centerSql))
@@ -29,30 +28,55 @@
     ?>
 </p>
 
-
-
 <p>Available Centers</p>
 <div class="d-flex flex-wrap py-2 gap-2">
     <?php
-    $centersql = "SELECT id,center FROM `centers` WHERE country = '$centerCountry' AND state = '$centerState' AND district = '$centerDist' AND tehsil = '$centerTeh' ORDER BY center ASC";
+    $centersql = "SELECT id, center FROM centers WHERE country = '$centerCountry' AND state = '$centerState' AND district = '$centerDist' AND tehsil = '$centerTeh' ORDER BY center ASC";
     $res = $conn->query($centersql);
+
+    $sr = 0;
+    $centerNumbers = [];
+
     while ($row = $res->fetch_assoc())
     {
+        if (preg_match('/\d+/', $row['center'], $matches))
+        {
+            $centerNumbers[] = (int) $matches[0];
+        }
         ?>
-        <div class="border border-danger bg-light shadow-sm p-2 px-3 rounded rounded-pill"><?php echo $row['center']; ?>
+        <div class="border border-danger bg-light shadow-sm p-2 px-3 rounded rounded-pill">
+            <?php echo $row['center']; ?>
             <a href="del_center.php?id=<?php echo $row['id']; ?>">
                 <button style="font-size:.8rem;" type="button" class="btn-close rounded rounded-pill bg-danger-subtle"
-                    aria-label="Close">
-                </button>
+                    aria-label="Close"></button>
             </a>
         </div>
         <?php
     }
+
+    if (!empty($centerNumbers))
+    {
+        $sr = max($centerNumbers) + 1;
+    } else
+    {
+        $sr = 1;
+    }
     ?>
-</div>
-<div class="">
-    <form class="d-flex" action="" method="post">
-        <input name="newCenter" class="form-control" type="text">
-        <input class="btn btn-outline-danger ml-2" name="centerAdd" type="submit" value="Add">
-    </form>
+    <div class="">
+        <a style="text-decoration:none;"
+            class="text-white border border-danger shadow-sm p-2 px-3 rounded rounded-pill btn btn-danger dropdown"
+            href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Add New
+        </a>
+        <ul class="dropdown-menu">
+            <form action="" method="post">
+                <li class="dropdown-item"><input name="newCenter" class="form-control" type="text"
+                        value="Center-<?php echo $sr; ?>">
+                </li>
+                <li class="dropdown-item"><input class="btn btn-outline-danger ml-2 form-control" name="centerAdd"
+                        type="submit" value="Add">
+                </li>
+            </form>
+        </ul>
+    </div>
 </div>
